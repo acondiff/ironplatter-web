62760fe672fbdc0ab6a2d6b7dc651e9bc0c2eb91
Austin Condiff <austin_condiff@intuit.com>
2017-02-14 16:23:14 -0600

    Added Heading title

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
+import './styles.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
